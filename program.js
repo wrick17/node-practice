519b7accbceca0c38bcd6d81e97fb1c90044135f
Pratyush Poddar <pratyush.poddar@compassitesinc.com>
2015-03-22 17:10:33 +0530

    upddated the routes to have restful names

--- program.js
+++ program.js
@@ -6,7 +6,7 @@ var list = [
   {'name': 'Pratyush', 'roll': '12'},
   {'name': 'Sayan', 'roll': '34'},
   {'name': 'Abhishek', 'roll': '38'},
-  {'name': 'Utsav', 'roll': '43'}
+  {'name': 'Utsav', 'roll': '43 '}
 ]
 
 var server = http.createServer(function (request, response) {
@@ -14,14 +14,15 @@ var server = http.createServer(function (request, response) {
 
   var parsedUrl = url.parse(request.url);
 
-  console.log('in create server -> ', parsedUrl.pathname);
+  console.log('in create server -> ', parsedUrl);
+  var parsedUrlPath = parsedUrl.pathname.split('/');
 
-  if (parsedUrl.pathname === '/getContacts' && request.method === 'GET') {
+  if (parsedUrlPath[1] === 'contacts' && parsedUrlPath.length < 3 && request.method === 'GET') {
     // response.write(JSON.stringify(list));
     response.writeHead(200, { 'Content-Type': 'application/json' });
     console.log(list);
     response.end(JSON.stringify(list));
-  } else if (parsedUrl.pathname === '/addContact' && request.method === 'POST') {
+  } else if (parsedUrlPath[1] === 'contacts' && parsedUrlPath.length < 3 && request.method === 'POST') {
     var body = '';
     request.on('data', function(data) {
       body += data;
@@ -32,7 +33,7 @@ var server = http.createServer(function (request, response) {
       console.log(list);
       response.end(JSON.stringify(list));
     });
-  } else if (parsedUrl.pathname === '/deleteContact' && request.method === 'POST') {
+  } else if (parsedUrlPath[1] === 'contacts' && parsedUrlPath.length === 3 && request.method === 'DELETE') {
     var body = '';
     request.on('data', function(data) {
       body += data;
@@ -40,7 +41,7 @@ var server = http.createServer(function (request, response) {
     request.on('end', function() {
       response.writeHead(200, { 'Content-Type': 'application/json' });
       var index = -1;
-      var name = qs.parse(body).name;
+      var name = parsedUrlPath[2];
       for (var i = 0; i < list.length; i++) {
         if (list[i].name === name) {
           index = i;
@@ -54,7 +55,7 @@ var server = http.createServer(function (request, response) {
       console.log(list);
       response.end(JSON.stringify(list));
     });
-  } else if (parsedUrl.pathname === '/editContact' && request.method === 'POST') {
+  } else if (parsedUrlPath[1] === 'contacts' && parsedUrlPath.length === 3 && request.method === 'PATCH') {
     var body = '';
     request.on('data', function(data) {
       body += data;
@@ -62,15 +63,16 @@ var server = http.createServer(function (request, response) {
     request.on('end', function() {
       response.writeHead(200, { 'Content-Type': 'application/json' });
       var index = -1;
-      var name = qs.parse(body).name;
+      var name = parsedUrlPath[2];
       var newname = qs.parse(body).newname;
+      var newroll = qs.parse(body).roll;
       for (var i = 0; i < list.length; i++) {
         if (list[i].name === name) {
           index = i;
         }
       };
       if (index !== -1) {
-        list.splice(index, 1, {'name': newname, 'roll': qs.parse(body).roll});
+        list.splice(index, 1, {'name': newname, 'roll': newroll});
       } else {
         console.log('man not found');
       }
